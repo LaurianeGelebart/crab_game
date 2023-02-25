923eb188c3def73d94597100bbf45ecb3e5a2e05
LaurianeG <lauriane.gelebart@gmail.com>
2023-02-25 09:14:58 +0100

    zones

---
+++ Crab/import_all.js
@@ -0,0 +1,53 @@
+// function import_all(){
+
+//     let data = {
+//         "clothing": [
+//           { // hat 
+//             "id": 1,
+//             "point": 20,
+//             "type": [
+//                 {
+//                     "id": 1,      
+//                     "image": "hat1.png"
+//                 },
+//                 {
+//                     "id": 2,      
+//                     "image": "hat2.png"
+//                 }
+//             ]
+//           },
+//           { // shoes 
+//             "id": 2,
+//             "point": 30,
+//             "type": [
+//                 {
+//                     "id": 1,      
+//                     "image": "shoes1.png"
+//                 },
+//                 {
+//                     "id": 2,      
+//                     "image": "shoes2.png"
+//                 }
+//             ]
+//           }, 
+//           { // glasses 
+//             "id": 3,
+//             "point": 10,
+//             "type": [
+//                 {
+//                     "id": 1,      
+//                     "image": "glasses1.png"
+//                 },
+//                 {
+//                     "id": 2,      
+//                     "image": "glasses2.png"
+//                 }
+//             ]
+//           }
+//         ]
+//     }
+
+// }
+
+// module.exports = {data};
+
